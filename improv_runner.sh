d2500c3665b097fdc01223665bb60062d1c3239d
nomsolence <nom@matcha.life>
2020-10-03 00:46:16 -0700

    backward shell

--- improv_runner.sh
+++ improv_runner.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "code improv has executed"
+# run hs.rennur_vorpmi, backwards
+while read line; do
+    eval "set -- $line"
+    if [ $# -ge 1 ]; then
+        eval "\$$(seq -s ' $' $# -1 1)"
+    fi
+done < hs.rennur_vorpmi
